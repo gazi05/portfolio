5841d445c9b4457fbc611d8a7ee72b286fbea11e
Ghazi <gazi.alqmmaz12@gmail.com>
2025-08-27 00:25:42 +0300

    /

--- src/sections/About.jsx
+++ src/sections/About.jsx
@@ -1,7 +1,7 @@
 function About(){
   return (
     <div className="ml-20">
-      <h1 className="text-white text-7xl mt-28 mb-24">Who is Ghazi ?</h1>
+      <h1 className="text-white text-7xl mt-28 mb-24">Who I Am </h1>
       <p className="text-white text-2xl w-[400px]  ">I am an AI student at Middle East University with a strong passion for web development. I enjoy creating modern, responsive websites using React and Tailwind CSS, combining functionality with clean, user-friendly design.</p>
       <hr className="border-t-2 border-white w-52 mt-12 " />
 
